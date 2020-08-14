07b507be129304f61218ce066b92a0733040f1ea
Sobinjm <jmsobin@gmail.com>
2020-08-14 14:36:01 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import {
+  Component,
+  OnInit,
+  Renderer2,
+  HostListener,
+  Inject
+} from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
+})
+export class AppComponent  {
+  title = 'Rexknar Website ';
+}
